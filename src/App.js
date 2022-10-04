fcf4a2f9309d9a41240bce7605075d3bc229e6ba
Sam Whitley <samwhitley@10-18-186-56.dynapool.wireless.nyu.edu>
2022-10-04 08:37:25 -0400

    setup React Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+//Importing React
+import React from "react";
+//Importing ReactDOM
+import ReactDOM from "react-dom/client";
+//Importing these packages from React DOM
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+//And lastly, importing Home
+import Home from './pages/Home';
+
+//Creating our path to the main page 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
       {/* Where React Router comes into play*/}
+      <RouterProvider router={router} />
     </div>
   );
 }
